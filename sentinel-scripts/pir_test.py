50fa09a7c7f800890ea7a1bd7c52af77691b210f
Sam Kelly <sam@conservationxlabs.org>
2019-12-03 20:08:24 +0000

    PIR testing

--- sentinel-scripts/pir_test.py
+++ sentinel-scripts/pir_test.py
@@ -1,12 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN)
-pir_status = GPIO.input(4)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 while 1:
+    pir_status = GPIO.input(4)
     if pir_status == 0:
          print('No Motion')
     if pir_status == 1:
        print('Motion Detected, awaiting confirmation')
-    time.sleep(1)
+    time.sleep(0.5)
