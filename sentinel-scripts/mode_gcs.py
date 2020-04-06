2b38463f95f24276eeb9dad7c7abfd73cb5f033b
Sam Kelly <samkelly5941@gmail.com>
2020-04-06 15:27:44 -0400

    df

--- sentinel-scripts/mode_gcs.py
+++ sentinel-scripts/mode_gcs.py
@@ -97,7 +97,7 @@ def ota_algorithm(user_array):
     alg_array  = 'gsutil cp gs://cxl_tflite/{}_config.csv ../models/{}_config.csv'.format(user_array[0],user_array[0])
     os.system(alg_array)
     alg_array = np.genfromtxt('../models/{}_config.csv'.format(user_array[0]))
-    print(alg_array)
+    print(alg_array[0][:])
     bucket = ''
     directory = ''
     model  = 'gsutil cp gs://{}/{} {}'.format(algorithm_name, bucket, directory)
