dc0010b9eb30b82dfd64b44277f5d10644109eaf
Sam Kelly <samkelly5941@gmail.com>
2020-04-06 16:38:13 -0400

    f

--- sentinel-scripts/mode_gcs.py
+++ sentinel-scripts/mode_gcs.py
@@ -97,7 +97,7 @@ def upload_images_gcp(directory,bucket):
 def ota_algorithm(user_array):
     alg_array  = 'gsutil cp gs://cxl_tflite/{}_config.csv ../models/{}_config.csv'.format(user_array[0],user_array[0])
     os.system(alg_array)
-    alg_array = np.loadtxt('../models/{}_config.csv'.format(user_array[0]), delimiter=',',dtype='str',skiprows=1)
+    alg_array = np.loadtxt('../models/{}_config.csv'.format(user_array[0]), delimiter=',',skiprows=1)
     print(alg_array)
     alg_rows = len(alg_array[0][:])
     print(alg_rows)
