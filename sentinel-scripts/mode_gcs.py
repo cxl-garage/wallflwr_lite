e5dbf771077cb73aa2dbccce6dde8c513a24b492
Sam Kelly <samkelly5941@gmail.com>
2020-04-09 18:16:55 -0400

    df

--- sentinel-scripts/mode_gcs.py
+++ sentinel-scripts/mode_gcs.py
@@ -130,7 +130,7 @@ def ota_algorithm(user_array):
                     #alg_array[k,13] = 'False'
         k = k+1
 
-    np.savetxt('../models','/{}_config.csv'.format(user_array[0]),alg_array,fmt="%s")
+    np.savetxt('../models','/{}_config.csv'.format(user_array[0]),alg_array)
     alg_array  = 'gsutil cp ../models/{}_config.csv gs://cxl_tflite/{}_config'.format(user_array[0],user_array[0])
     os.system(alg_array)
     print('Successful update')
