b0ca0adcf0b2a7240f4bc4f4ef590850fdd42d1a
Sam Kelly <samkelly5941@gmail.com>
2020-11-17 17:04:59 -0500

    attempt 6

--- sentinel-scripts/cloud_db.py
+++ sentinel-scripts/cloud_db.py
@@ -222,6 +222,7 @@ def device_info():
     URL = 'mysql+pymysql://{}:{}@{}/{}'.format(db_user,db_pass,db_ip,db_name)
     engine = sqlalchemy.create_engine(URL, pool_size=5,max_overflow=2,pool_timeout=30,pool_recycle=1800,)
     query = "SELECT * FROM devices WHERE device_name = \'{}\'".format(os.environ.get('device_name'))
+    print(query)
     device_information = pd.read_sql(query,con=engine)
     print(device_information)
     device_information = device_information.reset_index(drop=True)
