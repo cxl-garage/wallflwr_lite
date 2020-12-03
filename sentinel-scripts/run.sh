747c487b04765b2117cb5c8f6d5090115432d9e5
Patrick Combe <patrickeccombe@gmail.com>
2020-12-03 17:31:26 -0500

    d

--- sentinel-scripts/run.sh
+++ sentinel-scripts/run.sh
@@ -1,44 +1,15 @@
 cd /
 cd /home/pi/wallflwr_lite/sentinel-scripts
-# # ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
-# # until grep -q "Ready for new connections"; do ; sleep 1; done; echo 'Found'
-# # until grep -m 1 "Ready for new connections"; do ; sleep 1; done; echo 'Found'
-# # ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
-# # sleep 1
-# while ! grep -m1 'Ready for new connections' < ./log.out; do
-#     sleep 1
-#     echo Checking
-# done
-# echo Continue
 
-# (./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json & &) | grep -q "Ready for new connections"
-
-# python3 main.py
-
-# sleep 5
-# ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
-# sleep 5
-# python3 main.py
-
-
-# FILE_TO_WATCH=./log.out
-# SEARCH_PATTERN='Ready for new connections'
-
-# tail -f -n0 ${FILE_TO_WATCH} | grep -qe ${SEARCH_PATTERN}
-
-# if [ $? == 1 ]; then
-#     echo "Search terminated without finding the pattern"
-# fi
-
-# python3 main.py
-cd /
-cd /home/pi/wallflwr_lite/sentinel-scripts
+#This will start the cloud proxy
 bash cloud_proxy.sh
 
+#This while loop checks until the connection is made with the cloud SQL
 while ! grep -m1 'Ready for new connections' < ./log.out; do
     sleep 2
     echo Connecting...
 done
 
+#Once connected it will run the main.py script
 echo Connected to SQL
 python3 main.py
