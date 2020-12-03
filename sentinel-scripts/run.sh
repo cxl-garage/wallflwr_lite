bd63cd4550ed470f2d4ae9a0df49960517bd9a82
Patrick Combe <patrickeccombe@gmail.com>
2020-12-03 15:35:05 -0500

    de

--- sentinel-scripts/run.sh
+++ sentinel-scripts/run.sh
@@ -1,6 +1,11 @@
 cd /
 cd /home/pi/wallflwr_lite/sentinel-scripts
-sleep 5
 ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
-sleep 5
+PID=$! #catch the last PID, here from command1
+wait $PID #wait for command1, in background, to end
 python3 main.py
+
+# sleep 5
+# ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
+# sleep 5
+# python3 main.py
