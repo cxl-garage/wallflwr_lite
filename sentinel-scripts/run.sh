dc42c226cc624105889870480e41b52f61d90b4d
Patrick Combe <patrickeccombe@gmail.com>
2020-12-03 15:49:24 -0500

    d

--- sentinel-scripts/run.sh
+++ sentinel-scripts/run.sh
@@ -1,7 +1,7 @@
 cd /
 cd /home/pi/wallflwr_lite/sentinel-scripts
 ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
-until grep -m 1 "Ready for new connections"; do : ; done
+until grep -q "Ready for new connections"; do ; sleep 1; done; echo 'Found'
 python3 main.py
 
 # sleep 5
