54f2cd104b2044b10e2ce5487b093cdd7778d8e7
Patrick Combe <patrickeccombe@gmail.com>
2020-12-03 15:02:44 -0500

    deh

--- sentinel-scripts/run.sh
+++ sentinel-scripts/run.sh
@@ -1,5 +1,6 @@
 cd /
 cd /home/pi/wallflwr_lite/sentinel-scripts
+sleep 5
 ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
 sleep 5
 python3 main.py
