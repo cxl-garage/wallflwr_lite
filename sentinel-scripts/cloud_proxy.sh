8406c0d115fdab36bc480259fab4b0f79cc8be1f
Patrick Combe <patrickeccombe@gmail.com>
2020-12-03 16:41:13 -0500

    d

--- sentinel-scripts/cloud_proxy.sh
+++ sentinel-scripts/cloud_proxy.sh
@@ -2,11 +2,11 @@
 echo “Started bar.sh”
 cd /
 cd /home/pi/wallflwr_lite/sentinel-scripts
-./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json & > /tmp/server-log.txt &
+./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json 2>&1 >> log.txt & 
 
 
 
 
 
-command_1 2>&1 >> log.txt
+# command_1 2>&1 >> log.txt
 
