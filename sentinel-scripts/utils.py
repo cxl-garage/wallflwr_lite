73fe03feb245611866689c4a92657bc5332112a9
Sam Kelly <samkelly5941@gmail.com>
2020-12-03 16:11:44 -0500

    c

--- sentinel-scripts/utils.py
+++ sentinel-scripts/utils.py
@@ -91,7 +91,7 @@ def initialize(opt):
         if os.environ.get('release') == 'latest':
             print(commit_to_checkout)
             checkout_tag = 'latest'
-             _, commits = sorted([(tag.commit.committed_datetime, tag) for tag in repo.tags], reverse=True,)[0]
+            _, commits = sorted([(tag.commit.committed_datetime, tag) for tag in repo.tags], reverse=True,)[0]
             logger.info('Pulled {} version (SHA: {})'.format(checkout_tag,commits[0]))
         elif os.environ.get('release') == 'debug':
             logger.info('In Debug mode, Git is manually controlled!')
