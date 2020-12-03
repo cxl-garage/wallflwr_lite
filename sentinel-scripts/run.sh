a1a4c937f4fa66832701be1be7dde02d4e39d32f
Patrick Combe <patrickeccombe@gmail.com>
2020-12-03 15:45:46 -0500

    dd

--- sentinel-scripts/run.sh
+++ sentinel-scripts/run.sh
@@ -1,8 +1,7 @@
 cd /
 cd /home/pi/wallflwr_lite/sentinel-scripts
 ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
-PID=$! #catch the last PID, here from command1
-wait $PID #wait for command1, in background, to end
+until my_cmd | grep -m 1 "Ready for new connections"; do : ; done
 python3 main.py
 
 # sleep 5
