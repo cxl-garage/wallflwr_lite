ddf1d59457d9db15e0d0951a16044f0fa9336420
Patrick Combe <patrickeccombe@gmail.com>
2020-12-03 16:21:13 -0500

    d

--- sentinel-scripts/run.sh
+++ sentinel-scripts/run.sh
@@ -1,16 +1,16 @@
 cd /
 cd /home/pi/wallflwr_lite/sentinel-scripts
-./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
+# ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json &
 # until grep -q "Ready for new connections"; do ; sleep 1; done; echo 'Found'
 # until grep -m 1 "Ready for new connections"; do ; sleep 1; done; echo 'Found'
-# ./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json & > /tmp/server-log.txt &
+./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json & > /tmp/server-log.txt &
 # sleep 1
 # while ! grep -m1 'Ready for new connections' < /tmp/server-log.txt; do
 #     sleep 1
 # done
 # echo Continue
 
-(./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json & &) | grep -q "Ready for new connections"
+# (./cloud_sql_proxy -instances=sentinel-project-278421:us-east4:algorithm-library=tcp:1234 -credential_file=../../credentials/sentinelDB.json & &) | grep -q "Ready for new connections"
 
 python3 main.py
 
