1cd9dc23933f8cb728c64d15b1efa192590d02c8
Patrick Combe <patrickeccombe@gmail.com>
2020-12-04 13:27:40 -0500

    d

--- sentinel-scripts/run.sh
+++ sentinel-scripts/run.sh
@@ -14,18 +14,25 @@ if [ $COUNTER -lt 3 ]
 then
     #This will start the cloud proxy ONLY if there is internet
     bash cloud_proxy.sh
+
+    cd /
+    cd /home/pi/wallflwr_lite/sentinel-scripts
     
-    #This while loop checks until the connection is made with the cloud SQL
+        #This while loop checks until the connection is made with the cloud SQL
     while [  $COUNTER2 -lt 10 ] | [! grep -m1 'Ready for new connections' < ./log.out] ; do 
         sleep 2
         echo Connecting...
         echo $COUNTER2
         let COUNTER2=COUNTER2+1 
     done
-
 fi
 
-
+#This while loop checks until the connection is made with the cloud SQL
+while ! grep -m1 'Ready for new connections' < ./log.out; do
+    sleep 2
+    echo Connecting...
+done
 
 #Once connected it will run the main.py script
+echo Connected to SQL
 python3 main.py 
