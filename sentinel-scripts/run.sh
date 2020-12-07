14babc5245cbe1039ccddee4148fe67bdee62abc
Patrick Combe <patrickeccombe@gmail.com>
2020-12-07 15:43:12 -0500

    d

--- sentinel-scripts/run.sh
+++ sentinel-scripts/run.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
 cd /
 cd /home/pi/wallflwr_lite/sentinel-scripts
+
+#Logs the whole process
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>logFull.out 2>&1
+
 
 #Ping until we have internet (it will try for 30 seconds)
 COUNTER=0
