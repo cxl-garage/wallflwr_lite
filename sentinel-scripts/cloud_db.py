d4d27b2302e3822f9e32ae3fd3d096f26099424b
Sam Kellymkell <samkelly5941@gmail.com>
2020-12-08 12:57:45 -0500

    x

--- sentinel-scripts/cloud_db.py
+++ sentinel-scripts/cloud_db.py
@@ -279,7 +279,7 @@ def device_info():
     query = "SELECT * FROM insights WHERE device_id = \'{}\'".format(os.environ.get('device_id'))
     insights = pd.read_sql(query,con=engine)
     print(insights)
-    insights = insights['insight_id','alg_id','time_stamp','class_id','class','confidence','image_id','x_min','y_min','x_max','y_max','device_id','group_id','group_confidence']
+    insights = insights[['insight_id','alg_id','time_stamp','class_id','class','confidence','image_id','x_min','y_min','x_max','y_max','device_id','group_id','group_confidence']]
     insights['committed_sql'] = 1
     insights['committed_images'] = 1
     insights['committed_lora'] = 1
