692a9d864d74fb6d396c3d612c89d3bcefde4036
Sam Kellymkell <samkelly5941@gmail.com>
2020-12-08 17:43:12 -0500

    try

--- sentinel-scripts/lora.py
+++ sentinel-scripts/lora.py
@@ -54,10 +54,13 @@ def main(attempts=1):
 	ttn_config = TTN(devaddr, nwkey, app, country='US')
 
 	lora = TinyLoRa(spi, cs, irq, rst, ttn_config)
-	logger.info(insights)
+	logger.info(len(insights))
 	x =  insights[insights['committed_lora']==0]
+	logger.info(len(x))
 	x = x[x['class'] != 'blank']
+	logger.info(len(x))
 	x = x[x['group_id'] != 'NaN']
+	logger.info(len(x))
 	x = x.drop_duplicates(subset=['group_id'], keep='last')
 	x = x.reset_index()
 	logger.info(x)
