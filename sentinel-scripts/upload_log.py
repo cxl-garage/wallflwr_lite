88e114a7cce4efae7ebfc0bf68eca21fa51d1f3b
Patrick Combe <patrickeccombe@gmail.com>
2020-12-10 17:07:53 -0500

    d

--- sentinel-scripts/upload_log.py
+++ sentinel-scripts/upload_log.py
@@ -3,33 +3,15 @@
 
 # This code is currently proprietary, further licensing will be decided in the near future
 
-"""
-This script is designed to allow interface between the Sentinel Device and Google Cloud Platform's various products including:
-- Cloud Functions
-- Google Cloud Storage
-
-It does not include SQL database, which we currently host (11-9-2020) on Google Cloud (found on mysql.py)
-"""
-
 import os
-# import time
-# import numpy as np
-# import sys,select
-# import datetime as dt
-# import csv
-# import pandas as pd
-# import requests
-# import json
 import logging
 import datetime
 import pathlib
-
 filePath = pathlib.Path().absolute()
-
 logger = logging.getLogger('upload_log')
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
-logger.info('HELLOOOO')
+#Get Device ID
 f = open("../device.id", "r")
 lines = f.readlines()
 device_id = lines[0].rstrip()
@@ -46,3 +28,7 @@ os.rename('{}/logs/fullLog.out'.format(filePath),'{}/logs/{}.out'.format( filePa
 logger.info('Uploading log')
 query = 'gsutil -m cp -r -n "./logs/{}.out" "gs://insights-{}/logs/"'.format(dt_string,device_id)
 os.system(query)
+
+
+#Remove log
+os.rename('{}/logs/{}.out'.format( filePath, dt_string))
