5d2710e24f8f136e04b884c7c2c0392fba2d8b94
Sam Kelly <samkelly5941@gmail.com>
2020-12-10 16:47:00 -0500

    x

--- sentinel-scripts/edge_process.py
+++ sentinel-scripts/edge_process.py
@@ -83,7 +83,7 @@ def group_confidence_calculation():
     alg_df = pd.read_csv('../data/device_insights.csv')
 
     # Drop existing calculated confidences
-    alg_df = alg_df.isna(subset=['group_confidence'])
+    alg_df = alg_df.loc[~alg_df.index.isin(alg_df.dropna(subset=['group_confidence']).index)]
 
     # Find groups
     group_keys = alg_df.group_id.unique()
