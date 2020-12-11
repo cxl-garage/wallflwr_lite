a77ab179ccf8488062eefda50ddce247ea90bc3d
Sam Kelly <samkelly5941@gmail.com>
2020-12-11 16:40:02 -0500

    s

--- sentinel-scripts/lora.py
+++ sentinel-scripts/lora.py
@@ -47,7 +47,7 @@ def main(attempts=1):
 	nwkey = bytearray.fromhex(device_info['lora_nwkey'][0])
 	app   = bytearray.fromhex(device_info['lora_appkey'][0])
 
-	rfm69 = adafruit_rfm69.RFM69(spi, cs, reset, 915.0,baudrate=4000000)
+	rfm69 = adafruit_rfm69.RFM69(spi, cs, rst, 915.0,baudrate=4000000)
 	rfm69.send('Hello world!')
 
 	ttn_config = TTN(devaddr, nwkey, app, country='US')
