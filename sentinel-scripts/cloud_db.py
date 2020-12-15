6c971b8d0004c89795b29ee5e7b5fe583425152e
Sam Kelly <samkelly5941@gmail.com>
2020-12-15 12:09:59 -0500

    synced up cloud + local

--- sentinel-scripts/cloud_db.py
+++ sentinel-scripts/cloud_db.py
@@ -294,18 +294,14 @@ def insight_check():
     engine = sqlalchemy.create_engine(URL, pool_size=5,max_overflow=2,pool_timeout=30,pool_recycle=1800,)
     local_insights = pd.read_csv('../data/device_insights.csv')
     local_insights.set_index('insight_id')
-    print(local_insights)
     query = "SELECT * FROM insights WHERE device_id = \'{}\'".format(os.environ.get('device_id'))
     cloud_insights = pd.read_sql(query,con=engine)
     cloud_insights['committed_sql'] = 1
     cloud_insights['committed_images'] = 0
     cloud_insights['committed_lora'] = 1
     cloud_insights.set_index('insight_id')
-    print(cloud_insights)
     insights = pd.concat([local_insights,cloud_insights])
     insights = insights.drop_duplicates(subset=['insight_id'], keep='first')
     insights = insights.sort_values(by=['insight_id'])
     insights = insights[['committed_sql','committed_lora','committed_images','insight_id','alg_id','time_stamp','class_id','class','confidence','image_id','x_min','y_min','x_max','y_max','device_id','group_id','group_confidence']]
-    print(insights)
-    os.sys.exit()
     insights.to_csv('../data/device_insights.csv')
