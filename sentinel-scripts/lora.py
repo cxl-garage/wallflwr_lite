ab0053a43d68a8af016a852acbf5d2fcc6b1a2e6
Sam Kelly <samkelly5941@gmail.com>
2020-12-15 11:08:14 -0500

    s

--- sentinel-scripts/lora.py
+++ sentinel-scripts/lora.py
@@ -26,7 +26,7 @@ import logging
 logger = logging.getLogger('lora')
 
 
-def main(attempts=1,test):
+def main(attempts=1,test=False):
 	try:
 		insights = pd.read_csv('../data/device_insights.csv')
 	except Exception as e:
