92ba8d50bbec5fb31ae3aa9036a9b55de4dacdf4
Sam Kelly <samkelly5941@gmail.com>
2020-12-15 13:36:55 -0500

    s

--- sentinel-scripts/lora_receive.py
+++ sentinel-scripts/lora_receive.py
@@ -1,5 +1,19 @@
 from lora_utils import TTN, TinyLoRa
 
+
+# Set Pin Outs
+i2c = busio.I2C(board.SCL, board.SDA)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs  = digitalio.DigitalInOut(board.D27)
+rst = digitalio.DigitalInOut(board.D22)
+irq = digitalio.DigitalInOut(board.D23) #16
+reset = digitalio.DigitalInOut(board.D25)
+
+
+device_info = pd.read_csv('../_device_info.csv')
+devaddr = bytearray.fromhex(device_info['lora_devaddr'][0])#bytearray([ 0x26, 0x02, 0x1E, 0x47 ])
+nwkey = bytearray.fromhex(device_info['lora_nwkey'][0])
+app   = bytearray.fromhex(device_info['lora_appkey'][0])
 ttn_config = TTN(devaddr, nwkey, app, country='US')
 lora = TinyLoRa(spi, cs, irq, rst, ttn_config)
 lora.receive()
