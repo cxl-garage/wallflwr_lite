bd03c4220ce597c88003cac6a1e0b8e5eafb719b
Patrick Combe <patrickeccombe@gmail.com>
2021-01-05 15:27:03 -0500

    d

--- sentinel-scripts/upload_log.py
+++ sentinel-scripts/upload_log.py
@@ -2,7 +2,7 @@
 # Author: Sam Kelly
 
 # This code is currently proprietary, further licensing will be decided in the near future
-
+import utils
 from sqlalchemy import update
 import os
 import logging
@@ -44,21 +44,21 @@ device_id = lines[0].rstrip()
 logger.info(device_id)
 
 
-# # Rename
-# dt_string = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-# logger.info(dt_string)
-# os.rename('{}/logs/fullLog.out'.format(filePath),
-#           '{}/logs/{}.out'.format(filePath, dt_string))
+# Rename
+dt_string = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+logger.info(dt_string)
+os.rename('{}/logs/fullLog.out'.format(filePath),
+          '{}/logs/{}.out'.format(filePath, dt_string))
 
-# # Upload
-# logger.info('Uploading log')
-# query = 'gsutil -m cp -r -n "./logs/{}.out" "gs://insights-{}/logs/"'.format(
-#     dt_string, device_id)
-# os.system(query)
+# Upload
+logger.info('Uploading log')
+query = 'gsutil -m cp -r -n "./logs/{}.out" "gs://insights-{}/logs/"'.format(
+    dt_string, device_id)
+os.system(query)
 
 
-# # Remove log
-# os.remove('{}/logs/{}.out'.format(filePath, dt_string))
+# Remove log
+os.remove('{}/logs/{}.out'.format(filePath, dt_string))
 
 # Get device info again
 db_user = os.environ.get("DB_USER")
@@ -71,6 +71,12 @@ engine = sqlalchemy.create_engine(
     URL, pool_size=5, max_overflow=2, pool_timeout=30, pool_recycle=1800,)
 query = "SELECT * FROM devices WHERE device_id = \'{}\'".format(
     device_id)
-print(query)
+# print(query)
 device_information = pd.read_sql(query, con=engine)
-print(device_information)
+# print(device_information)
+cycle_time = str(device_information['cycle_time'][0])
+shutdown = str(device_information['shutdown'][0])
+
+# Shut down Raspberry Pi
+if shutdown == '1':
+    utils.shutdown(cycle_time)
