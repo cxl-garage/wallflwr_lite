a6b01c9e408fd267e37b0c1f081477302f5bbe2b
Patrick Combe <patrickeccombe@gmail.com>
2021-01-05 13:30:33 -0500

    d

--- sentinel-scripts/main.py
+++ sentinel-scripts/main.py
@@ -1,5 +1,5 @@
 ##### Tyto AI: Conservation X Labs   #####
-## Author: Sam Kelly
+# Author: Sam Kelly
 
 # This code is currently proprietary, further licensing will be decided in the near future
 
@@ -10,33 +10,45 @@ This script is the main process that controls the Sentinel Device in the field
 import argparse
 import requests
 parser = argparse.ArgumentParser()
-parser.add_argument('--test', action='store_true', help='Use onboard test data rather than SD card')
-parser.add_argument('--wilderness', action='store_true', help='No internet scenario')
+parser.add_argument('--test', action='store_true',
+                    help='Use onboard test data rather than SD card')
+parser.add_argument('--wilderness', action='store_true',
+                    help='No internet scenario')
 parser.add_argument('--lora_off', action='store_true', help='Disable LoRa')
-parser.add_argument('--gcs_off', action='store_true', help='Disable Upload of Pictures')
-parser.add_argument('--sql_off', action='store_true', help='Disable Upload to SQL DB')
-parser.add_argument('--update_off', action='store_true', help='Disable update of algorithms')
-parser.add_argument('--type', type=str, default='int8_edgetpu', help='type of model')
-parser.add_argument('--tpu_off', action='store_true', help='Turn off TPU (just run on CPU)')
-parser.add_argument('--text', action='store_true', help='Send text notification')
-parser.add_argument('--email', action='store_true', help='Send email notification')
+parser.add_argument('--gcs_off', action='store_true',
+                    help='Disable Upload of Pictures')
+parser.add_argument('--sql_off', action='store_true',
+                    help='Disable Upload to SQL DB')
+parser.add_argument('--update_off', action='store_true',
+                    help='Disable update of algorithms')
+parser.add_argument('--type', type=str,
+                    default='int8_edgetpu', help='type of model')
+parser.add_argument('--tpu_off', action='store_true',
+                    help='Turn off TPU (just run on CPU)')
+parser.add_argument('--text', action='store_true',
+                    help='Send text notification')
+parser.add_argument('--email', action='store_true',
+                    help='Send email notification')
 opt = parser.parse_args()
 
-### Checking if device has internet access
+# Checking if device has internet access
+
+
 def connect(url='http://www.google.com/', timeout=3):
     try:
         r = requests.head(url, timeout=timeout)
         return True
     except requests.ConnectionError as ex:
-        #print(ex)
+        # print(ex)
         return False
+
 
 k = 0
 while 1:
     try:
         import logging
         import os
-        ## Setting relative path (necessary for backseat driving)
+        # Setting relative path (necessary for backseat driving)
         try:
             os.chdir("/home/pi/wallflwr_lite/sentinel-scripts")
         except:
@@ -78,7 +90,6 @@ while 1:
         k = k + 1
 
 
-
 # Initialize the device (check that local device is ready)
 data_directory = utils.initialize(opt)
 
@@ -106,24 +117,23 @@ if data_directory != 'error':
     primary_algs = pd.read_csv('../models/_primary_algs.txt')
     secondary_algs = pd.read_csv('../models/_secondary_algs.txt')
 
+    # Process data until there are no data left in the data directory
+    # while len(os.listdir(data_directory)) != 0:
 
-    ## Process data until there are no data left in the data directory
-    #while len(os.listdir(data_directory)) != 0:
-
-    ## Running loop of all algorithms that have to run on device
+    # Running loop of all algorithms that have to run on device
     k = 0
     while k < len(primary_algs):
         primary_alg = primary_algs.iloc[[k]]
         logger.info('Model {} Starting'.format(primary_alg['alg_id'][0]))
 
         # Run Primary ALgorithm
-        primary_df = edge_process.main(primary_alg,data_directory,opt.type)
+        primary_df = edge_process.main(primary_alg, data_directory, opt.type)
         logger.info('Model {} Complete'.format(primary_alg['alg_id'][0]))
 
         # Run Secondary Model (if it exists)
-        #if len(secondary_algs)! :
+        # if len(secondary_algs)! :
         #    secondary_df = edge_process.main(secondary_algs,data_directory,opt.type)
-            #print('Insert outcome from secondary model:')# secondary_class, secondary_confidence)
+        # print('Insert outcome from secondary model:')# secondary_class, secondary_confidence)
 
         k = k+1
 
@@ -143,13 +153,12 @@ if connect() == True and opt.wilderness != True:
         cloud_data.upload_images()
         # cloud_data.upload_log()
 
-
     # Send email notification (if requested by SQL table eventually)
-    if opt.email ==True:
+    if opt.email == True:
         cloud_data.notification(type=email)
 
     # Send text notification (if requested by SQL table eventually)
-    if opt.text ==True:
+    if opt.text == True:
         cloud_data.notification(type=text)
 
 else:
@@ -160,8 +169,8 @@ else:
 # Delete all processed files from SD Card
 utils.delete_files()
 
-## Shut down Raspberry Pi
-if os.environ.get("cycle_time") == '1':
-    utils.shutdown(os.environ.get("cycle_time"))
+# Shut down Raspberry Pi
+if os.environ.get("shutdown") == '1':
+    utils.shutdown(os.environ.get("shutdown"))
 else:
     logger.info('Processing complete, device idling (shutdown disabled)')
