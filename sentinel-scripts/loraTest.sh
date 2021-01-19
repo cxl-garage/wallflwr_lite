f32375038e42ab588b0ae9911330babdc6bce9e4
Patrick Combe <patrickeccombe@gmail.com>
2021-01-19 14:55:53 -0500

    d

--- sentinel-scripts/loraTest.sh
+++ sentinel-scripts/loraTest.sh
@@ -6,5 +6,4 @@ bash cloud_proxy.sh
 
 
 printf '\n\n\n\n\n RUNNING WITH NO INTERNET / LORA \n\n\n\n\n\n\n'
-sudo gsutil -m cp -r gs://sentinel_test_data/ ../data/camera/DCIM
 python3 main.py --wilderness
