5fce5ad9f0153cf611d6a1b4494f960369e68623
Patrick Combe <patrickeccombe@gmail.com>
2021-01-21 16:26:55 -0500

    s

--- sentinel-scripts/edge_process.py
+++ sentinel-scripts/edge_process.py
@@ -389,10 +389,11 @@ def main(alg, data_directory, quantize_type, algorithm_type='detection', batch=1
             # Loading the model into tensorflow engine
             if os.environ.get('version').startswith('0'):
                 interpreter = DetectionEngine(model)
-            if os.environ.get('version').startswith('1'):
+                break
+            elif os.environ.get('version').startswith('1'):
                 interpreter = make_interpreter(model)
                 interpreter.allocate_tensors()
-            break
+                break
         except Exception as e:
             logging.error('No TPU Found. Rebooting...')
             # utils.shutdown(0)
